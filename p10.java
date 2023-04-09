9d8974c098c7f78dd8a2b9506a35536fe11f1370
Hasnain Makada <hasnainmakada@gmail.com>
2023-04-09 11:02:52 +0530

    update

--- p10.java
+++ p10.java
@@ -1,17 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class p10 {
-    static int reverse(int number) {
-        int reverseNumber = 0;
-        while (number > 0) {
-            reverseNumber = reverseNumber * 10 + number % 10;
-            number /= 10;
+
+    static int[] reverse(int[] numbers) {
+        int[] newReversed = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            newReversed[i] = newReversed[i] * 10 + numbers[i] % 10;
+            numbers[i] /= 10;
         }
-        return reverseNumber;
+
+        return newReversed;
     }
 
     public static void main(String[] args) {
-        int reversedNumber = reverse(1234567);
-        System.out.println(reversedNumber);
+        int[] numbers = new int[10];
+
+        System.out.println("Enter 10 numbers : ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(reverse(numbers)));
+        scanner.close();
     }
 
 }
--- p7.java
+++ p7.java
@@ -1,9 +1,9 @@
-
 public class p7 {
     public static void main(String[] args) {
         String pNumber = " " + (char) ((Math.random() * 26) + 65);
         pNumber += (char) ((Math.random() * 26) + 65);
         pNumber += (char) ((Math.random() * 26) + 65);
+        pNumber += (int) (Math.random() * 10);
         pNumber += (int) (Math.random() * 10);
         pNumber += (int) (Math.random() * 10);
         pNumber += (int) (Math.random() * 10);
